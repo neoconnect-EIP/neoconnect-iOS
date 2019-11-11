8b4183421a6e3fb6dd4d70aa40ea81e4c02ba00f
Yassine <yassine.toutouh@epitech.eu>
2019-11-11 15:29:31 +0800

    First commit : Login/Register form Shop/Inf done and work well

---
+++ NeoConnect/Brands/Connected/HomePage/B_HomeViewController.swift
@@ -0,0 +1,32 @@
+//
+//  B_HomeViewController.swift
+//  NeoConnect
+//
+//  Created by EIP on 10/09/2019.
+//  Copyright © 2019 EIP. All rights reserved.
+//
+
+import UIKit
+
+class B_HomeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBOutlet weak var OfferStackView: UIStackView!
+    
+    
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ NeoConnect/Brands/Password Forgotten/B_ForgotPasswordViewController.swift
@@ -0,0 +1,35 @@
+//
+//  ForgotPasswordViewController.swift
+//  NeoConnect
+//
+//  Created by EIP on 01/11/2019.
+//  Copyright © 2019 EIP. All rights reserved.
+//
+
+import UIKit
+
+class B_ForgotPasswordViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBOutlet weak var emailTextField: UITextField!
+    
+    
+    @IBAction func sendButtonTapped(_ sender: Any) {
+        
+    }
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
